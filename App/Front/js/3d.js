5347c9096e2065161ba262a1d38c0c63dbcbbb99
adaminbrussels <adam.weir@student.ehb.be>
2020-01-08 15:37:51 +0100

    15:37

--- App/Front/js/3d.js
+++ App/Front/js/3d.js
@@ -4,34 +4,47 @@ $(document).ready(function() {
 const scene = new THREE.Scene(),
     camera = new THREE.PerspectiveCamera(
     75,
-    window.innerWidth / window.innerHeight,
+    $('body').width() / $('body').height(),
     0.1, 1000
 ),
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer({alpha:true});
 
-renderer.setSize(
-    window.innerWidth,
-    window.innerHeight
-);
 $('body').append(renderer.domElement);
 
 
 
 /* Main */
-// Define the scene
+
+// Define the texture
+const texture = new THREE.TextureLoader().load("././img/test.jpg",
+    function(texture) {
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    }
+);
+
+// Define the geometry
 const geometry = new THREE.BoxGeometry(1,1,1),
-    material = new THREE.MeshBasicMaterial({color:0xff0040});
+    material = new THREE.MeshBasicMaterial({map:texture});
+    material.side = THREE.BackSide;
     cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
     cube.rotation.x = 0.5;
     
-    camera.position.z = 2;
+    camera.position.z = 1.5;
     
-    // Render
-    function animate() {
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-        cube.rotation.y += 0.02;
+// Render
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    cube.rotation.y += 0.005;
+
+    // Make responsive
+    renderer.setSize(
+        window.innerWidth,
+        window.innerHeight
+    );
+    camera.aspect = $('body').width() / $('body').height();
+    camera.updateProjectionMatrix();
 }
 
 animate();
